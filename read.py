c87dffa5805b45f4ef95d83a2443a88b6fbc01ff
Winnie Lin <winnie771737@gmail.com>
2022-07-30 00:17:57 +0800

    calculate average reviews length

--- read.py
+++ read.py
@@ -6,4 +6,10 @@ with open('reviews.txt', 'r') as f:
 		count += 1
 		if count % 1000 == 0:
 			print(len(data))
+print('檔案讀取完了，總共有', len(data), '筆資料')
 
+sum_length = 0
+for d in data:
+	sum_length += len(d)
+average = sum_length / 1000000
+print('留言的平均長度為:', average)
